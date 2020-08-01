b8639cebed5aa4f19ac361f561d066a756a91336
sikny <pamartqu@gmail.com>
2020-08-02 00:00:52 +0200

    Spline draw in editor

---
+++ Editor/SplinePointPropertyDrawer.cs
@@ -0,0 +1,23 @@
+﻿using SplineEditor.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace SplineEditor.Editor {
+    [CustomPropertyDrawer(typeof(SplinePoint))]
+    public class SplinePointPropertyDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            
+            var vectorRect = new Rect(position.x, position.y, position.width, position.height);
+            SplinePoint splinePoint = (SplinePoint) property.objectReferenceValue;
+            if(splinePoint == null)
+                EditorGUI.LabelField(vectorRect, "Null Reference");
+            else
+                splinePoint.transform.position = EditorGUI.Vector3Field(vectorRect, GUIContent.none, splinePoint.transform.position);
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Runtime/ControlPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace SplineEditor.Runtime {
+    public class ControlPoint : MonoBehaviour {
+        [HideInInspector] public SplinePoint splinePoint;
+
+        private void OnDrawGizmos() {
+            if (Selection.activeTransform is null || !Selection.activeTransform.IsChildOf(splinePoint.transform)
+                || splinePoint.isLast) return;
+            
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(transform.position, SplineUtils.ControlPointSize);
+        }
+    }
+}
