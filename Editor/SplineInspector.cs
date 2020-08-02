bdc148a8bcb625087df2c03604538ba7057f6c01
sikny <pamartqu@gmail.com>
2020-08-02 03:31:24 +0200

    control points in spline inspector + background colors

--- Editor/SplineInspector.cs
+++ Editor/SplineInspector.cs
@@ -7,32 +7,42 @@ using UnityEngine;
 namespace SplineEditor.Editor {
     [CustomEditor(typeof(Spline))]
     public class SplineInspector : UnityEditor.Editor {
+        private readonly Color _red = Color.red;
+        private readonly Color _entryBg = new Color(0.5f,0.5f,0.5f);
+
+        private Color _tmp;
         public override void OnInspectorGUI() {
             serializedObject.Update();
             var list = serializedObject.FindProperty("points");
-            
+
             EditorGUILayout.PropertyField(list, false);
             EditorGUI.indentLevel += 1;
             if (list.isExpanded) {
                 GUILayout.BeginHorizontal();
                 EditorGUILayout.LabelField("Length : " + list.FindPropertyRelative("Array.size").intValue);
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button(new GUIContent("+", "Add"), EditorStyles.miniButton, 
-                    GUILayout.Width(30))) {
-                    //++list.arraySize;
+                if (GUILayout.Button("+", GUILayout.Width(100))) {
                     ((Spline) serializedObject.targetObject).AddPoint();
                 }
                 GUILayout.EndHorizontal();
                 int digits = (int) Math.Floor(Math.Log10(list.arraySize-1) + 1f);
                 if (digits < 0) digits = 1;
                 for (int i = 0; i < list.arraySize; i++) {
-                    EditorGUILayout.BeginHorizontal();
+                    GUIStyle style = new GUIStyle();
+                    Texture2D tex = new Texture2D(1, 1);
+                    tex.SetPixel(0, 0, _entryBg);
+                    tex.Apply();
+                    style.normal.background = tex;
+                    style.margin.bottom = 5;
+                    EditorGUILayout.BeginHorizontal(style);
                     EditorGUILayout.LabelField("" + i, GUILayout.Width(20f + digits * 8f));
                     EditorGUILayout.PropertyField(list.GetArrayElementAtIndex(i), GUIContent.none);
-                    if (GUILayout.Button(new GUIContent("-", "Remove element"), EditorStyles.miniButton,
-                        GUILayout.Width(30))) {
+                    _tmp = GUI.backgroundColor;
+                    GUI.backgroundColor = _red;
+                    if (GUILayout.Button("-", GUILayout.Width(30))) {
                         ((Spline) serializedObject.targetObject).RemovePoint(i);
                     }
+                    GUI.backgroundColor = _tmp;
                     EditorGUILayout.EndHorizontal();
                 }
             }
--- Editor/SplinePointPropertyDrawer.cs
+++ Editor/SplinePointPropertyDrawer.cs
@@ -9,15 +9,36 @@ namespace SplineEditor.Editor {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             EditorGUI.BeginProperty(position, label, property);
             position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            float labelWidth = 100f;
             
             var vectorRect = new Rect(position.x, position.y, position.width, position.height);
+            
+            var ctrl1LblRect = new Rect(position.x, position.y, labelWidth, position.height);
+            var ctrl1Rect = new Rect(position.x + labelWidth, position.y + 22f, position.width - labelWidth, position.height);
+            
+            var ctrl2LblRect = new Rect(position.x, position.y + 22f, labelWidth, position.height);
+            var ctrl2Rect = new Rect(position.x + labelWidth, position.y + 44f, position.width - labelWidth, position.height);
+            
             SplinePoint splinePoint = (SplinePoint) property.objectReferenceValue;
             if(splinePoint == null)
                 EditorGUI.LabelField(vectorRect, "Null Reference");
-            else
-                splinePoint.transform.position = EditorGUI.Vector3Field(vectorRect, GUIContent.none, splinePoint.transform.position);
-
+            else {
+                splinePoint.transform.localPosition =
+                    EditorGUI.Vector3Field(vectorRect, GUIContent.none, splinePoint.transform.localPosition);
+                
+                EditorGUI.LabelField(ctrl1LblRect, "Control point 1");
+                splinePoint.controlPoint1.localPosition =
+                    EditorGUI.Vector3Field(ctrl1Rect, GUIContent.none, splinePoint.controlPoint1.localPosition);
+                EditorGUI.LabelField(ctrl2LblRect, "Control point 2");
+                splinePoint.controlPoint2.localPosition =
+                    EditorGUI.Vector3Field(ctrl2Rect, GUIContent.none, splinePoint.controlPoint2.localPosition);
+            }
             EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            return base.GetPropertyHeight(property, label) * 3f + 10f;
         }
     }
 }
