e44fa7941b569f0afc2f5f19b41fef46c3c16f2a
sikny <pamartqu@gmail.com>
2020-11-28 18:18:58 +0100

    Bezier curve with color settings ready

---
+++ Editor/BezierCurveEditor.cs
@@ -0,0 +1,90 @@
+﻿using SplineEditor.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace SplineEditor.Editor {
+    [CustomEditor(typeof(BezierCurve))]
+    public class BezierCurveEditor : UnityEditor.Editor {
+        private int _selectedPoint; // 0, 1 -> start/end point; 2, 3 -> start/end tangent
+
+        private void OnSceneGUI() {
+            BezierCurve be = target as BezierCurve;
+            if (be == null) {
+                Debug.LogError("Possible NullReferenceException on BezierCurve");
+                return;
+            }
+            
+            Handles.color = be.settings.tangentLinesColor;
+            Handles.DrawDottedLine(be.startPoint, be.startTangent, 2);
+            Handles.DrawDottedLine(be.endPoint, be.endTangent, 2);
+            
+            Handles.color = be.settings.bezierPointColor;
+            if (Handles.Button(be.startPoint, Quaternion.identity, 0.1f, 0.1f, Handles.CubeHandleCap)) {
+                _selectedPoint = 0;
+            }
+            if (Handles.Button(be.endPoint, Quaternion.identity, 0.1f, 0.1f, Handles.CubeHandleCap)) {
+                _selectedPoint = 1;
+            }
+            Handles.color = be.settings.bezierControlPointColor;
+            if (Handles.Button(be.startTangent, Quaternion.identity, 0.1f, 0.1f, Handles.SphereHandleCap)) {
+                _selectedPoint = 2;
+            }
+            if (Handles.Button(be.endTangent, Quaternion.identity, 0.1f, 0.1f, Handles.SphereHandleCap)) {
+                _selectedPoint = 3;
+            }
+            
+            var position = GetSelectedPoint(be);
+            if (position.HasValue) {
+                EditorGUI.BeginChangeCheck();
+                var newPos = Handles.PositionHandle(position.Value, Quaternion.identity);
+
+                if (EditorGUI.EndChangeCheck()) {
+                    Undo.RecordObject(target, "Changed Bezier point");
+                    SetSelectedPoint(be, newPos);
+                }
+            }
+        }
+
+        private Vector3? GetSelectedPoint(BezierCurve be) {
+            switch (_selectedPoint) {
+                case 0:
+                    return be.startPoint;
+                case 1:
+                    return be.endPoint;
+                case 2:
+                    return be.startTangent;
+                case 3:
+                    return be.endTangent;
+                default:
+                    return null;
+            }
+        }
+
+        private void SetSelectedPoint(BezierCurve be, Vector3 position) {
+            switch (_selectedPoint) {
+                case 0:
+                    be.startPoint = position;
+                    break;
+                case 1:
+                    be.endPoint = position;
+                    break;
+                case 2:
+                    be.startTangent = position;
+                    break;
+                case 3:
+                    be.endTangent = position;
+                    break;
+            }
+        }
+
+        void OnEnable() {
+            Tools.hidden = true;
+            _selectedPoint = 0;
+        }
+
+        void OnDisable() {
+            Tools.hidden = false;
+        }
+    }
+}
---
+++ Runtime/BezierSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace SplineEditor.Runtime {
+    [CreateAssetMenu(fileName = "BezierSettings", menuName = "ScriptableObjects/BezierSettings")]
+    public class BezierSettings : ScriptableObject {
+        public Color bezierCurveColor;
+        public Color bezierPointColor;
+        public Color bezierControlPointColor;
+        public Color tangentLinesColor;
+    }
+}
