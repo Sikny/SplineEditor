fb6642b18928812d4cb8bc4705b4fcf4b811c5ef
Quentin <pamartqu@gmail.com>
2020-12-18 08:48:47 +0100

    fix different normals on same bezier, + inspector for tangents (in local pos)

--- Assets/Runtime/Scripts/BezierControlPoint.cs
+++ Assets/Runtime/Scripts/BezierControlPoint.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 // ReSharper disable once CheckNamespace
 namespace SplineEditor.Runtime {
     [Serializable]
-    public class BezierControlPoint {
+    public class BezierControlPoint
+    {
         public Vector3 position;
-        private Vector3 _tangent1;
-        private Vector3 _tangent2;
+        [SerializeField] private Vector3 tangent1;
+        [SerializeField] private Vector3 tangent2;
 
         public Vector3 Tangent1
         {
-            get => _tangent1 + position;
+            get => tangent1 + position;
             set
             {
                 var val = value - position;
-                _tangent1 = val;
-                _tangent2 = -val;
+                tangent1 = val;
+                tangent2 = -val;
             }
         }
 
         public Vector3 Tangent2
         {
-            get => _tangent2 + position;
+            get => tangent2 + position;
             set {
                 var val = value - position;
-                _tangent2 = val;
-                _tangent1 = -val;
+                tangent2 = val;
+                tangent1 = -val;
             }
         }
     }
